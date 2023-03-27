cc2643f4ecc73b7af0f57a5ac84a5e8d77e46e89
DJGValls <jiga1980@gmail.com>
2023-03-27 11:04:22 +0200

    only one vote per a day added to votes route

--- routes/player.routes.js
+++ routes/player.routes.js
@@ -38,6 +38,10 @@ router.post("/create-player", isAuthenticated, async (req, res, next) => {
 router.patch("/:playerId/votes", isAuthenticated, async (req, res, next) => {
   const { portero, defensa, tecnica, ataque, cardio } = req.body;
   const { playerId } = req.params;
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth();
+  const currentDay = date.getDate();
 
   // No fields are empty
   if (!portero || !defensa || !tecnica || !ataque || !cardio) {
@@ -48,14 +52,17 @@ router.patch("/:playerId/votes", isAuthenticated, async (req, res, next) => {
 
   try {
     const foundPlayer = await Player.findById(playerId);
+    const lastUpdatedDate = foundPlayer.updatedAt;
+    const lastUpdatedYear = lastUpdatedDate.getFullYear();
+    const lastUpdatedMonth = lastUpdatedDate.getMonth();
+    const lastUpdatedDay = lastUpdatedDate.getDate();
+
     //  no votes for your own player profiles
     if (foundPlayer.user === req.payload._id) {
-      return res
-        .status(400)
-        .json({
-          errorMessage:
-            "No puedes votar a perfiles de jugador que te pertenezcan",
-        });
+      return res.status(400).json({
+        errorMessage:
+          "No puedes votar a perfiles de jugador que te pertenezcan",
+      });
     }
     // aplied only first time the player receives a votation
     if (foundPlayer.votes === 0) {
@@ -68,14 +75,28 @@ router.patch("/:playerId/votes", isAuthenticated, async (req, res, next) => {
         votes: foundPlayer.votes + 1,
       });
     } else {
-      await Player.findByIdAndUpdate(playerId, {
-        portero: ((foundPlayer.portero + Number(portero)) / 2).toFixed(2),
-        defensa: ((foundPlayer.defensa + Number(defensa)) / 2).toFixed(2),
-        tecnica: ((foundPlayer.tecnica + Number(tecnica)) / 2).toFixed(2),
-        ataque: ((foundPlayer.ataque + Number(ataque)) / 2).toFixed(2),
-        cardio: ((foundPlayer.cardio + Number(cardio)) / 2).toFixed(2),
-        votes: foundPlayer.votes + 1,
-      });
+      // 1 vote per day
+      console.log(lastUpdatedDate);
+      console.log(date);
+      console.log(currentYear, lastUpdatedYear);
+      if (
+        currentYear === lastUpdatedYear &&
+        currentMonth === lastUpdatedMonth &&
+        currentDay === lastUpdatedDay
+      ) {
+        return res
+          .status(400)
+          .json({ errorMessage: "Solo puedes votar una vez al dia" });
+      } else {
+        await Player.findByIdAndUpdate(playerId, {
+          portero: ((foundPlayer.portero + Number(portero)) / 2).toFixed(2),
+          defensa: ((foundPlayer.defensa + Number(defensa)) / 2).toFixed(2),
+          tecnica: ((foundPlayer.tecnica + Number(tecnica)) / 2).toFixed(2),
+          ataque: ((foundPlayer.ataque + Number(ataque)) / 2).toFixed(2),
+          cardio: ((foundPlayer.cardio + Number(cardio)) / 2).toFixed(2),
+          votes: foundPlayer.votes + 1,
+        });
+      }
     }
   } catch (error) {
     next(error);
@@ -99,7 +120,6 @@ router.patch("/:playerId/votes", isAuthenticated, async (req, res, next) => {
 
 // GET "/api/player/:playerId"
 router.get("/:playerId", isAuthenticated, async (req, res, next) => {
-  // console.log(req.params.id);
   const { playerId } = req.params;
   try {
     const foundPlayer = await Player.findById(playerId);
@@ -111,22 +131,23 @@ router.get("/:playerId", isAuthenticated, async (req, res, next) => {
 });
 
 // DELETE "/api/player/:playerId/delete"
-router.delete("/:playerId/delete", isAuthenticated, async (req,res,next)=>{
-  const { playerId } = req.params
+router.delete("/:playerId/delete", isAuthenticated, async (req, res, next) => {
+  const { playerId } = req.params;
   try {
-    const foundPlayer = await Player.findById(playerId)
-    
-    if (req.payload._id == foundPlayer.user) {
-      
-      await Player.findByIdAndDelete(playerId)
-      return res.status(200).json("personaje borrado")
-    } else{
-      return res.status(400).json({ errorMessage: "No puedes borrar un perfil de jugador que no te pertenezca" });
-    }
+    const foundPlayer = await Player.findById(playerId);
 
+    if (req.payload._id == foundPlayer.user) {
+      await Player.findByIdAndDelete(playerId);
+      return res.status(200).json("personaje borrado");
+    } else {
+      return res.status(400).json({
+        errorMessage:
+          "No puedes borrar un perfil de jugador que no te pertenezca",
+      });
+    }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 module.exports = router;
